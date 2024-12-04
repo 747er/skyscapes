4175fdc2d6c14a504af64fcb947220a7002d8978
codifynw <cody@codifynw.com>
2024-12-04 12:30:26 -0800

    prog

--- src/sections/gallery/gallery.js
+++ src/sections/gallery/gallery.js
@@ -1,7 +1,9 @@
 import PhotoSwipe from 'photoswipe';
 
+console.log('sanity')
+
 document.addEventListener('DOMContentLoaded', () => {
-  const galleryElements = document.querySelectorAll('.photoswipe-gallery a');
+  const galleryElements = document.querySelectorAll('#gallery--photoswipe-gallery a');
 
   const items = Array.from(galleryElements).map(link => ({
     src: link.getAttribute('href'),
@@ -9,10 +11,14 @@ document.addEventListener('DOMContentLoaded', () => {
     h: parseInt(link.getAttribute('data-pswp-height')),
   }));
 
+  console.log('items: ', items);
+
   const gallery = new PhotoSwipe({
     dataSource: items,
     gallerySelector: '.photoswipe-gallery',
   });
+
+  console.log('gallery: ', gallery);
 
   gallery.init();
 });
