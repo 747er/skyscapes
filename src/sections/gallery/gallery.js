cb67d06316354ad6665e39692507169503b69412
codifynw <cody@codifynw.com>
2024-12-04 13:36:00 -0800

    bg fix

--- src/sections/gallery/gallery.js
+++ src/sections/gallery/gallery.js
@@ -2,21 +2,17 @@ import PhotoSwipeLightbox from 'photoswipe/lightbox';
 import PhotoSwipe from 'photoswipe';
 import "photoswipe/style.css";
 
-console.log('sanity check');
-
 if (document.readyState === 'loading') {
+    console.log('loading');
   document.addEventListener('DOMContentLoaded', initializeGallery);
 } else {
   // DOM is already ready
+  console.log('already ready');
   initializeGallery();
 }
 
 function initializeGallery() {
-  console.log('DOM is ready');
-
   const galleryElements = document.querySelectorAll('#gallery--photoswipe-gallery a');
-
-  console.log('Found elements! ', galleryElements);
 
   if (galleryElements.length) {
     const items = Array.from(galleryElements).map(link => ({
@@ -25,8 +21,6 @@ function initializeGallery() {
       h: parseInt(link.getAttribute('data-pswp-height')),
     }));
 
-    console.log('Gallery items: ', items);
-
     const lightbox = new PhotoSwipeLightbox({
         gallery: '#gallery--photoswipe-gallery',
         children: 'a',
@@ -34,8 +28,6 @@ function initializeGallery() {
     });
 
     lightbox.init();
-    // gallery.init();
-    console.log('Gallery initialized');
   } else {
     console.log('No gallery elements found');
   }
