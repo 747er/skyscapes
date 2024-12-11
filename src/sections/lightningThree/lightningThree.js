945a7a05c99c7459f13376835094152b6e387eee
codifynw <cody@codifynw.com>
2024-12-11 13:46:40 -0800

    dynamic flash

--- src/sections/lightningThree/lightningThree.js
+++ src/sections/lightningThree/lightningThree.js
@@ -1,7 +1,7 @@
 import * as THREE from "three";
-import texture1Url from './rain1.jpg';
-import texture2Url from './rain2.jpg';
-import texture3Url from './rain3.jpg';
+import texture1Url from "./rain1.jpg";
+import texture2Url from "./rain2.jpg";
+import texture3Url from "./rain3.jpg";
 
 /***************************************************** Scene */
 
@@ -47,7 +47,7 @@ container.appendChild(renderer.domElement);
 // });
 
 window.addEventListener("resize", () => {
-    console.log('resize fire')
+  console.log("resize fire");
   const width = container.clientWidth;
   const height = container.clientHeight;
 
@@ -173,6 +173,51 @@ for (let p = 0; p < 25; p++) {
   scene.add(cloud3);
 }
 
+/***************************************************** Events */
+// Function to trigger the lightning flash
+// function triggerFlash() {
+//     // Initial bright flash
+//     const initialPower = 50 + Math.random() * 500;
+//     flash.power = initialPower;
+
+//     // Slightly dim it after 50ms
+//     setTimeout(() => {
+//       flash.power = initialPower * 0.5; // half brightness
+//     }, 50);
+
+//     // Then turn it off after another 100ms (total 150ms)
+//     setTimeout(() => {
+//       flash.power = 0;
+//     }, 150);
+// }
+
+function triggerFlash() {
+  // Randomize position for variety:
+  // For example, center around x=0 by subtracting half the range:
+  const xPos = Math.random() * 800 - 400; // from -400 to 400
+  const yPos = 300 + Math.random() * 200; // from 300 to 500
+  const zPos = 100;
+  flash.position.set(xPos, yPos, zPos);
+
+  // Initial bright flash
+  const initialPower = 50 + Math.random() * 500;
+  flash.power = initialPower;
+
+  // Slightly dim after 50ms
+  setTimeout(() => {
+    flash.power = initialPower * 0.5;
+  }, 50);
+
+  // Turn it off after another 100ms (total ~150ms)
+  setTimeout(() => {
+    flash.power = 0;
+  }, 150);
+}
+
+// Listen for mouse events on the container
+container.addEventListener("mouseenter", triggerFlash);
+container.addEventListener("mouseleave", triggerFlash);
+
 /***************************************************** Render */
 
 // Render animation on every rendering phase
@@ -191,11 +236,11 @@ function render() {
   });
 
   // Lightening Animation: Random the flash position and light intensity
-  if (Math.random() > 0.93 || flash.power > 100) {
-    if (flash.power < 100)
-      flash.position.set(Math.random() * 400, 300 + Math.random() * 200, 100);
-    flash.power = 50 + Math.random() * 500;
-  }
+  //   if (Math.random() > 0.93 || flash.power > 100) {
+  //     if (flash.power < 100)
+  //       flash.position.set(Math.random() * 400, 300 + Math.random() * 200, 100);
+  //     flash.power = 50 + Math.random() * 500;
+  //   }
 
   // rerender every time the page refreshes (pause when on another tab)
   requestAnimationFrame(render);
